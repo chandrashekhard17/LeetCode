ca23c4d872db3b7538c0a3e49fab88a402abf8ff
Chandrashekhar D <135732078+chandrashekhard17@users.noreply.github.com>
2024-08-28 00:42:11 +0530

    Time: 42 ms (63.21%), Space: 12.4 MB (73.42%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+# Example usage
+solution = Solution()
+
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(solution.twoSum(nums1, target1))  # Output: [0, 1]
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(solution.twoSum(nums2, target2))  # Output: [1, 2]
+
+nums3 = [3, 3]
+target3 = 6
+print(solution.twoSum(nums3, target3))  # Output: [0, 1]
